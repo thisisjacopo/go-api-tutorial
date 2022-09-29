f2fe0e1b0ce4e0ec2bea68582558089454f042d5
Jacopo <jacopo.rodighiero@icloud.com>
2022-09-29 15:49:29 +0200

    chore: added getBookById with err handling

---
+++ README.md
@@ -0,0 +1,3 @@
+To -> run this project : go run main.go
+To -> getBooks / run :  curl localhost:8080/books
+To -> createBook / run :  curl localhost:8080/books --include --header "Content-Type: application/json" -d @body.json --request "POST"
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import(
 	"net/http"
 	"github.com/gin-gonic/gin"
-	// "errors"
+	"errors"
 )
 
 type book struct {
@@ -20,9 +20,29 @@ var books = []book{
 	{ID: "4", Title: "Title number 4", Author: "Author number 4", Quantity: 54},
 }
 
+
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
+	
+}
 
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
 }
 
 func createBook(c *gin.Context) {
@@ -40,5 +60,6 @@ func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
 	router.POST("/books", createBook)
+	router.GET("/books/:id", bookById)
 	router.Run("localhost:8080")
 }

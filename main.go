e86a40fb8f4a7cfe932da225d23b0f8562ce0ee3
Jacopo <jacopo.rodighiero@icloud.com>
2022-09-29 13:41:03 +0200

    chore: added post route

--- main.go
+++ main.go
@@ -3,21 +3,21 @@ package main
 import(
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"errors"
+	// "errors"
 )
 
 type book struct {
-	ID		string	`json: "id"`
-	Title 	string	`json: "title"`
-	Author	string	`json: "author"`
-	Quantity int	`json: "quantity"`
+	ID		 string	`json:"id"`
+	Title 	 string	`json:"title"`
+	Author	 string	`json:"author"`
+	Quantity int	`json:"quantity"`
 }
 
 var books = []book{
-	{ID: "1", Title: "Title number 1", Author: "Author number 1", Quantity: 2}
-	{ID: "2", Title: "Title number 2", Author: "Author number 2", Quantity: 21}
-	{ID: "3", Title: "Title number 3", Author: "Author number 3", Quantity: 10}
-	{ID: "4", Title: "Title number 4", Author: "Author number 4", Quantity: 54}
+	{ID: "1", Title: "Title number 1", Author: "Author number 1", Quantity: 2},
+	{ID: "2", Title: "Title number 2", Author: "Author number 2", Quantity: 21},
+	{ID: "3", Title: "Title number 3", Author: "Author number 3", Quantity: 10},
+	{ID: "4", Title: "Title number 4", Author: "Author number 4", Quantity: 54},
 }
 
 func getBooks(c *gin.Context) {
@@ -25,8 +25,20 @@ func getBooks(c *gin.Context) {
 
 }
 
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	router := gin.Default()
-	rounter.GET("/books", getBooks)
-	rounter.Run("localhost:8080")
+	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
+	router.Run("localhost:8080")
 }
